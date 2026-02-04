50f99c040d3a223ccb21865d871758ae2994b829
Aidar Isenov <aidarisenov31@gmail.com>
2026-02-05 00:45:20 +0500

    add customer & ticket models and factories

---
+++ database/factories/TicketFactory.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace Database\Factories;
+
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TicketFactory extends Factory 
+{
+    public function definition(): array 
+    {
+        return [
+            'customer_id' => Customer::factory(),
+            'topic' => $this->faker->sentence(5),
+            'body' => $this->faker->paragraph(5),
+            'status' => $this->faker->randomElement(['new', 'in_work', 'done'])
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Customer;
+use App\Models\Ticket;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,7 +18,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
         $this->call([
             RoleSeeder::class,
         ]);
@@ -27,5 +30,10 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $user->assignRole(['manager']);
+
+
+        Customer::factory(10)
+        ->has(Ticket::factory()->count(rand(2, 5)))
+        ->create();
     }
 }
